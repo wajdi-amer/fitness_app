0385195b2f7bcbc5b4aa9942cb08baf1eb4e5980
wajdi-amer <wajdi-amer@cyber4s.dev>
2022-09-16 21:30:19 +0300

    individual icon for each body part

--- client/src/components/BodyPart.js
+++ client/src/components/BodyPart.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
-import dumbbellIcon from '../assets/icons/dumbbell-icon.png';
 import '../index.css';
+import chestIcon from '../assets/icons/chest.png';
+import backIcon from '../assets/icons/back.png';
+import lowerArmsIcon from '../assets/icons/lower-arms.png';
+import upperArmsIcon from '../assets/icons/upper-arms.png';
+import lowerLegsIcon from '../assets/icons/lower-legs.png';
+import upperLegsIcon from '../assets/icons/upper-legs.png';
+import shouldersIcon from '../assets/icons/shoulders.png';
+import cardioIcon from '../assets/icons/cardio.png';
+import neckIcon from '../assets/icons/neck.png';
+import waistIcon from '../assets/icons/waist.png';
+import fullBodyIcon from '../assets/icons/fullbody.png';
+
+const icons = {
+  'all': fullBodyIcon,
+  'chest': chestIcon,
+  'back': backIcon,
+  'lower arms': lowerArmsIcon,
+  'upper arms': upperArmsIcon,
+  'lower legs': lowerLegsIcon,
+  'upper legs': upperLegsIcon,
+  'shoulders': shouldersIcon,
+  'cardio': cardioIcon,
+  'neck': neckIcon,
+  'waist': waistIcon,
+}
 
 const BodyPart = ({item, bodyPart, setBodyPart}) => {
   return (
@@ -12,11 +36,15 @@ const BodyPart = ({item, bodyPart, setBodyPart}) => {
     className='bodyPart-card'
     sx={{
         borderTop: bodyPart === item ? '3px solid rgb(33,37,41)' : '',
+        borderBottom: bodyPart === item ? '3px solid rgb(33,37,41)' : '',
         width: '170px',
         height: '150px'
     }}
+    onClick={() => {
+      setBodyPart(item)
+    }}
     >
-        <img src={dumbbellIcon} alt='dumbbell' style={{width: '90px', height: '90px'}}/>
+        <img src={icons[item]} alt={item+'-icon'} style={{width: '90px', height: '90px'}}/>
         <Typography fontWeight='bold' textTransform='capitalize'>{item}</Typography>
     </Stack>
   )
